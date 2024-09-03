086c3be54b614c5f882c06e7cf06cf0a39fb1c6b
zjyezaire <zaire.elleby@gmail.com>
2024-09-03 16:15:57 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const pokemon = require('./data.js')
+
+const game = {
+  party: [],
+  gyms: [
+    { location: "Pewter City", completed: false, difficulty: 1 },
+    { location: "Cerulean City", completed: false, difficulty: 2 },
+    { location: "Vermilion City", completed: false, difficulty: 3 },
+    { location: "Celadon City", completed: false, difficulty: 4 },
+    { location: "Fuchsia City", completed: false, difficulty: 5 },
+    { location: "Saffron City", completed: false, difficulty: 6 },
+    { location: "Cinnabar Island", completed: false, difficulty: 7 },
+    { location: "Viridian City", completed: false, difficulty: 8 },
+  ],
+  items: [
+    { name: "potion", quantity: 4 },
+    { name: "pokeball", quantity: 8 },
+    { name: "rare candy", quantity: 99 },
+  ],
+}
+
+
+// Exercise 1 
+//log JUSAT the name of the Pokemon with the number 59
+
+console.log(pokemon[58])
